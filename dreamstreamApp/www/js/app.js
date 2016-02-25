de9fa2d26b4e9ad5136f709cc0a8108746720193
Ian Smith <iansmith1026@gmail.com>
2016-02-25 14:37:28 -0700

    merged

--- dreamstreamApp/www/js/app.js
+++ dreamstreamApp/www/js/app.js
@@ -56,6 +56,7 @@ angular.module('dreamstreamApp', ['ionic', 'dreamstreamApp.controllers', 'dreams
       }
     })
     .state('tab.new', {
+      cache: false,
       url: '/new',
       views: {
         'new': {
@@ -65,6 +66,7 @@ angular.module('dreamstreamApp', ['ionic', 'dreamstreamApp.controllers', 'dreams
       }
     })
     .state('tab.data', {
+      cache: false,
       url: '/data',
       views: {
         'data': {
