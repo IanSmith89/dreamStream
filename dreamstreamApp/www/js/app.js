4fbc5bf9827d52701d7c6208dbc1a792948eb143
Saundie Weiss <saundieweiss@gmail.com>
2016-03-04 13:42:09 -0700

    Added firebase hosting scripts and changed d3 and highcharts to https

--- dreamstreamApp/www/js/app.js
+++ dreamstreamApp/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('dreamstreamApp', ['ionic', 'highcharts-ng', 'dreamstreamApp.controllers', 'dreamstreamApp.services', 'ngMessages'])
+angular.module('dreamstreamApp', ['ionic', 'highcharts-ng', 'dreamstreamApp.controllers', 'dreamstreamApp.services', 'ngMessages', 'firebase'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
